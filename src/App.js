3601fa35050f5f7aeaada2385311560c9352e9a0
irshadvali <irshad.vali@gmail.com>
2017-10-04 16:06:49 +0530

    navigation drawer removed

--- src/App.js
+++ src/App.js
@@ -5,22 +5,22 @@ import { StackNavigator, DrawerNavigator } from "react-navigation";
 import { AppRegistry, StyleSheet, Text, View } from "react-native";
 
 
-const fortigoStack = StackNavigator({
+const projectStack = StackNavigator({
   /* we can change it to  TabNavigator and DrawerNavigator*/
   // Login: { screen: Login },
   Home: { screen: AccountListContainer },
 
 });
 
-const drawerNavigator = new DrawerNavigator(
-  {
-    /* we can change it to  TabNavigator and DrawerNavigator*/
-    Home: { screen: fortigoStack }
-  },
-  {
-    drawerPosition: "left",
-    contentComponent: props => <SldeMenu {...props} />
-  }
-);
+// const drawerNavigator = new DrawerNavigator(
+//   {
+//     /* we can change it to  TabNavigator and DrawerNavigator*/
+//     Home: { screen: projectStack }
+//   },
+//   {
+//     drawerPosition: "left",
+//     contentComponent: props => <SldeMenu {...props} />
+//   }
+// );
 
-export default drawerNavigator;
+export default projectStack;
